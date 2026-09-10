05260995b083134f4d592408d0be0656453227fb
BabishaBJ248 <babishabj8043@gmail.com>
2026-09-10 14:13:45 +0530

    Time: 0 ms (100%), Space: 9.3 MB (32.25%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.c
@@ -0,0 +1,35 @@
+int search(int* nums, int numsSize, int target) {
+    int low=0;
+    int high=numsSize-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(target==nums[mid])
+        {
+          return mid;
+        }
+        else if(nums[low]<=nums[mid])
+        {
+         if(nums[low] <=target && nums[mid]>=target)
+         {
+            high=mid-1;
+         }
+         else
+         {
+            low=mid+1;
+         }
+        }
+        else
+        {
+         if(nums[high] >= target && nums[mid] <=target)
+         {
+            low=mid+1;
+         }
+         else
+         {
+            high=mid-1;
+         }
+        }
+    }
+    return -1;
+}
